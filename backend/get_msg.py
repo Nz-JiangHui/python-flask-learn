7c8146eb800918981858924d9e904e03d037a5a6
jianghui <nz.jianghui@gmail.com>
2018-09-15 17:09:18 +0800

    backend

---
+++ backend/get_msg.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"/getMsg": {"origins": "*"}})
+
+@app.route('/')
+def hello_world():
+  return 'Hello World!'
+
+@app.route('/getMsg', methods=['GET', 'POST'])
+def home():
+  response = {
+    'msg': 'Hello, Python !'
+  }
+  return jsonify(response)
+
+if __name__ == '__main__':
+  app.run()
